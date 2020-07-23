565d1b56e575391ce91640456799500150770579
Pedro Costa <pedro_babysco@outlook.com>
2020-07-23 17:24:05 -0300

    Testando estrutura de dados inicial

--- fogo.js
+++ fogo.js
@@ -3,11 +3,16 @@ const fireWidht = 2;
 const fireHeight = 3;
 
 function start() {
-
+    createFireDataStructure();
+    console.log(firePixelArray);
 }
 
 function createFireDataStructure() {
+    const numberOfPixels = fireWidht * fireHeight;
 
+    for ( let i = 0; i < numberOfPixels; i++) {
+        firePixelArray[i] = 0;
+    }
 }
 
 function calculateFirePropagation() {
