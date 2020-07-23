4b9a80d1396c885aabbb63843c3b4a158baeccbc
Pedro Costa <pedro_babysco@outlook.com>
2020-07-23 18:59:16 -0300

    Função de renderizar concluida

--- fogo.js
+++ fogo.js
@@ -1,26 +1,59 @@
+// Variaveis principais
 const firePixelArray = [];
 const fireWidht = 2;
 const fireHeight = 3;
 
+// Função para inicializar
 function start() {
     createFireDataStructure();
-    console.log(firePixelArray);
+    renderFire();
 }
 
+// Função para criar a estrutura de dados do fogo
 function createFireDataStructure() {
+    // Numero de pixels
     const numberOfPixels = fireWidht * fireHeight;
 
+    // laço para preencher a estrutura de dados 
     for ( let i = 0; i < numberOfPixels; i++) {
         firePixelArray[i] = 0;
     }
 }
 
+// Função para calcular a propagação do fogo
 function calculateFirePropagation() {
 
 }
 
+// Funçao para renderizar o fogo
 function renderFire() {
+    // Criando uma table
+    let html = '<table cellpadding=0 cellspacing=0>';
 
+    // Pra cada pixel da altura do fogo cria uma <tr>
+    for (let row = 0; row < fireHeight; row++) {
+        html += '<tr>';
+
+        // Pra cada largura do fogo cria uma <td.
+        for (let column = 0; column < fireWidht; column++) {
+            // Indice do pixel
+            const pixelIndex = column + (fireWidht * row);
+
+            html += '<td>';
+
+            // Printando o valor dentro da célula
+            html += pixelIndex;
+
+            html += '</td>';
+        }
+
+        html += '</tr>';
+    }
+
+    html += '</table>';
+
+    // Substituindo o innerHTML pelo o resultado de tudo
+    document.querySelector('#fireCanvas').innerHTML = html;
 }
 
 start();
