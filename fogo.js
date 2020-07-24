0f6b31f4153a15498c6f2387e984c099769c1560
Pedro Costa <pedro_babysco@outlook.com>
2020-07-23 21:04:32 -0300

    Renderizando com a cot de acordo com a intensidade

--- fogo.js
+++ fogo.js
@@ -2,6 +2,8 @@
 const firePixelArray = [];
 const fireWidht = 10;
 const fireHeight = 10;
+// Paleta de cor
+const fireColorsPalette = [{"r":7,"g":7,"b":7},{"r":31,"g":7,"b":7},{"r":47,"g":15,"b":7},{"r":71,"g":15,"b":7},{"r":87,"g":23,"b":7},{"r":103,"g":31,"b":7},{"r":119,"g":31,"b":7},{"r":143,"g":39,"b":7},{"r":159,"g":47,"b":7},{"r":175,"g":63,"b":7},{"r":191,"g":71,"b":7},{"r":199,"g":71,"b":7},{"r":223,"g":79,"b":7},{"r":223,"g":87,"b":7},{"r":223,"g":87,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":95,"b":7},{"r":215,"g":103,"b":15},{"r":207,"g":111,"b":15},{"r":207,"g":119,"b":15},{"r":207,"g":127,"b":15},{"r":207,"g":135,"b":23},{"r":199,"g":135,"b":23},{"r":199,"g":143,"b":23},{"r":199,"g":151,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":159,"b":31},{"r":191,"g":167,"b":39},{"r":191,"g":167,"b":39},{"r":191,"g":175,"b":47},{"r":183,"g":175,"b":47},{"r":183,"g":183,"b":47},{"r":183,"g":183,"b":55},{"r":207,"g":207,"b":111},{"r":223,"g":223,"b":159},{"r":239,"g":239,"b":199},{"r":255,"g":255,"b":255}] ;
 
 // Função para inicializar
 function start() {
@@ -57,11 +59,14 @@ function updateFireIntesityPerPixel(currentPixelIndex){
 
 // Funçao para renderizar o fogo
 function renderFire() {
+    const debug = false;
+
     // Criando uma table
     let html = '<table cellpadding=0 cellspacing=0>';
 
     // Pra cada pixel da altura do fogo cria uma <tr>
     for (let row = 0; row < fireHeight; row++) {
+        
         html += '<tr>';
 
         // Pra cada largura do fogo cria uma <td.
@@ -69,16 +74,25 @@ function renderFire() {
             // Indice do pixel
             const pixelIndex = column + (fireWidht * row);
             // Caessando valor contido dentro da estrutura de dados
-            const fireintesity = firePixelArray[pixelIndex];
+            const fireIntesity = firePixelArray[pixelIndex];
 
-            html += '<td>';
+            if (debug == true) {
+                html += '<td>';
 
-            // Imprimindo o valor dentro da célula
-            html += `<div class="pixel-index">${pixelIndex}</div>`
-            // Imprimindo o valor da estrura de dados 
-            html += fireintesity;
+                // Imprimindo o valor dentro da célula
+                html += `<div class="pixel-index">${pixelIndex}</div>`
+                // Imprimindo o valor da estrura de dados 
+                html += fireIntesity;
 
-            html += '</td>';
+                html += '</td>';
+            } else {
+                const color = fireColorsPalette[fireIntesity];
+                const colorString = `${color.r},${color.g},${color.b}`;
+                html += `<td style="background-color: rgb(${colorString})">`;
+                html += '</td>';
+            }
+
+            
         }
 
         html += '</tr>';
